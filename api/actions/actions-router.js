afe5c9a990d13dfd9b0a5dc92adae1d3a407adc6
Ade <shennaike2386@hotmail.com>
2022-04-15 03:59:27 -0400

    server and router base is set and running

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,34 @@
 // Write your "actions" router here!
+const express = require('express');
+const Actions = require('./actions-model')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+
+})
+
+router.get('/:id', (req, res, next) => {
+
+})
+
+router.post('/', (req, res, next) => {
+
+})
+
+router.put('/:id', (req, res, next) => {
+
+})
+
+router.delete('/:id', (req, res, next) => {
+
+})
+
+router.use((err, req, res, next) => {// eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message || "Whoops, looks like the internet broke!",
+        stack: err.stack
+    })
+})
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,38 @@
 // Write your "projects" router here!
 const express = require('express');
+const Projects = require('./projects-model')
 
 const router = express.Router()
+
+router.get('/', (req, res, next) => {
+
+})
+
+router.get('/:id', (req, res, next) => {
+
+})
+
+router.post('/', (req, res, next) => {
+
+})
+
+router.put('/:id', (req, res, next) => {
+
+})
+
+router.delete('/:id', (req, res, next) => {
+
+})
+
+router.get('/:id/actions', (req, res, next) => {
+
+})
+
+router.use((err, req, res, next) => {// eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message || "Whoops, looks like the internet broke!",
+        stack: err.stack
+    })
+})
 
 module.exports = router
