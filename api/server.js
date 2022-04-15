5554f3f27f03b7e90d2398dab5ef5a41e13cbf29
Ade <shennaike2386@hotmail.com>
2022-04-15 03:37:52 -0400

    initial

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json())
+
+const projectRouter = require('./projects/projects-router')
+
+server.use('/api/projects', projectRouter)
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const server = require('./api/server')
+
+const port = process.env.PORT || 9000 
+
+server.listen(port, () => {
+    console.log(`\n**Server running on localhost:${port}**\n`)
+})
 /*
 play this: https://www.youtube.com/watch?v=d-diB65scQU
 
