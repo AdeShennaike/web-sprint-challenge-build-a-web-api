173134591a995d5480ef86fcd2884545c1a7a14f
Ade <shennaike2386@hotmail.com>
2022-04-15 17:54:20 -0400

    wip

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -2,14 +2,25 @@
 const Projects = require('./projects-model')
 
 function validateId(req, res, next) {
-    
- console.log(validateId)
- next()
+    Projects.get(req.params.id)
+    .then(project => {
+        if(!project){
+            res.status(404).json({message: 'Project not found'})
+        }else{
+            next()
+        }
+    }) 
 }
 
 function validateBody(req, res, next) {
- console.log(validateBody)
- next()    
+    const {name, description} = req.body
+    
+    if(!name || !description){
+        res.status(400).json({ 
+            message: "body is missing any of the required"})
+    }else{
+        next()
+    }
 }
 
 module.exports = {
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -5,11 +5,10 @@ const Projects = require('./projects-model')
 const {validateId, validateBody} = require('./projects-middleware')
 
 const router = express.Router()
-console.log(Projects)
 
 router.get('/', (req, res, next) => {
     Projects.get()
-        .then(project => {
+    .then(project => {
             res.status(200).json(project)
         })
         .catch(err => {
@@ -17,7 +16,7 @@ router.get('/', (req, res, next) => {
         })
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', validateId, (req, res, next) => {
     Projects.get(req.params.id)
         .then(project => {
             res.status(200).json(project)
@@ -27,11 +26,30 @@ router.get('/:id', (req, res, next) => {
         })
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', validateBody, async (req, res, next) => {
+    const {name, description} = req.body
 
+    try{
+        const newProject = await Projects.insert({name: name, description: description, completed: true})
+        res.status(201).json(newProject)
+    }catch(err){
+        next(err)
+    }
+    // const newProject = await Projects.insert({name: name, description: description, completed: true })
+    // const {id} = newProject
+    // const project = await Projects.get(id)
+    // res.status(201).json(project)
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', validateId, validateBody, async (req, res, next) => {
+    const {name, description} = req.body
+     
+    try{
+        const projectChange = await Projects.update(req.params.id, {name: name, description: description, completed: true})
+        res.status(200).json(projectChange)
+    }catch(err){
+        next(err)
+    }
 
 })
 
