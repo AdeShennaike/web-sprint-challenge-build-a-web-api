7b26c6d5e59f36b0e04a9042e1fffa48f24d2bea
Ade <shennaike2386@hotmail.com>
2022-04-15 22:14:02 -0400

    middleware for actions is created and works and get, get by id and delete endpoints for actions router work.

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,10 +1,28 @@
 // add middlewares here related to actions
-// const Actions = require('./actions-model')
+const Actions = require('./actions-model')
 
-// function validateId(req, res, next) {}
-// function validateBody(req, res, next) {}
+function validateId(req, res, next) {
+    Actions.get(req.params.id)
+    .then(action => {
+        if(!action){
+            res.status(404).json({message: 'Action not found'})
+        }else{
+            next()
+        }
+    }) 
+}
 
-// module.exports = {
-//     validateId,
-//     validateBody
-// }
+function validateBody(req, res, next) {
+    const {project_id, description, notes, completed} = req.body
+    
+    if(!notes || !description || completed === undefined){
+        res.status(400).json({ 
+            message: "body is missing any of the required"})
+    }else{
+        next()
+    }
+}
+module.exports = {
+    validateId,
+    validateBody
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,17 +1,29 @@
 // Write your "actions" router here!
 const express = require('express');
 const Actions = require('./actions-model')
-// const {validateId, validateBody} = require('./actions-middlware')
+const {validateId, validateBody} = require('./actions-middlware')
 
 const router = express.Router()
 
-// router.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
+    Actions.get()
+    .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            next(err)
+        })
+})
 
-// })
-
-// router.get('/:id', (req, res, next) => {
-
-// })
+router.get('/:id', validateId, (req, res, next) => {
+    Actions.get(req.params.id)
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            next(err)
+        })
+})
 
 // router.post('/', (req, res, next) => {
 
@@ -21,9 +33,15 @@ const router = express.Router()
 
 // })
 
-// router.delete('/:id', (req, res, next) => {
-
-// })
+router.delete('/:id', validateId, (req, res, next) => {
+    Actions.remove(req.params.id)
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(err => {
+            next(err)
+        })
+})
 
 router.use((err, req, res, next) => {// eslint-disable-line
     res.status(err.status || 500).json({
